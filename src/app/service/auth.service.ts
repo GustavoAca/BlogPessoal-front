fd67150fffaab412bf51550833a226f6926a934c
Gustavo dos Anjos <galasdalas50@gmail.com>
2022-06-07 19:03:57 -0300

    Implementando get e post de temas

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -16,22 +16,26 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  cadastrar(user:User): Observable <User>{
-    return this.http.post<User>("http://localhost:8080/usuarios/cadastrar", user)
+  cadastrar(user: User): Observable<User> {
+    return this.http.post<User>("https://gustablogpessoal.herokuapp.com/usuarios/cadastrar", user);
+
+    // return this.http.post<User>("http://localhost:8080/usuarios/cadastrar", user);
 
   }
 
   entrar(userLogin: UserLogin): Observable<UserLogin> {
-      return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://gustablogpessoal.herokuapp.com/usuarios/logar', userLogin);
+
+    // return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin);
   }
 
-  logado(){
+  logado() {
     let ok = false;
 
-if(environment.token != '' ){
-  ok = true;
+    if (environment.token != '') {
+      ok = true;
 
-}
+    }
 
 
     return ok;
